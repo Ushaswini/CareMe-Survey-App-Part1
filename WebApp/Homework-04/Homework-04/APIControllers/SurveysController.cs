dc5fde726f30885cab23030dfd5f67735f051afe
Ushaswini <ushaswini.vinnakota@gmail.com>
2017-11-10 07:51:20 -0500

    Homework-04 Submission

---
+++ WebApp/Homework-04/Homework-04/APIControllers/SurveysController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Homework_04.Models;
+using Homework_04.DTOs;
+
+namespace Homework_04.Controllers
+{
+    [RoutePrefix("api/Surveys")]
+    public class SurveysController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Surveys
+        public IList<SurveyDTO> GetSurveys()
+        {
+            return db.Surveys.Include(s => s.StudyGroup).Select(s => new SurveyDTO {
+                        SurveyId = s.SurveyId,
+                        SurveyCreatedTime = s.SurveyCreatedTime,
+                        QuestionText = s.QuestionText,
+                        StudyGroupId = s.StudyGroupId,
+                        StudyGroupName = s.StudyGroup.StudyName
+
+            }).ToList();
+        }
+
+        // GET: api/Surveys/5
+        [ResponseType(typeof(Survey))]
+        public IHttpActionResult GetSurvey(string id)
+        {
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(survey);
+        }
+
+        // PUT: api/Surveys/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSurvey(string id, Survey survey)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != survey.SurveyId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(survey).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SurveyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Surveys
+        [ResponseType(typeof(Survey))]
+        public IHttpActionResult PostSurvey(Survey survey)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Surveys.Add(survey);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (SurveyExists(survey.SurveyId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = survey.SurveyId }, survey);
+        }
+
+        // DELETE: api/Surveys/5
+        [ResponseType(typeof(Survey))]
+        public IHttpActionResult DeleteSurvey(string id)
+        {
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return NotFound();
+            }
+
+            db.Surveys.Remove(survey);
+            db.SaveChanges();
+
+            return Ok(survey);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SurveyExists(string id)
+        {
+            return db.Surveys.Count(e => e.SurveyId == id) > 0;
+        }
+    }
+}
---
+++ WebApp/Homework-04/Homework-04/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Homework_04.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.Title = "Dashboard";
+
+            return View("~/Views/Admin/Dashboard.cshtml");
+        }
+    }
+}
